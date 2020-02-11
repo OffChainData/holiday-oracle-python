1a7d6027579f14c40c9eb5f56ca89fe1c9e96d02
Julian Broudou <julian.broudou@gmail.com>
2020-02-11 14:39:24 +1100

    removed scripts

--- setup.py
+++ setup.py
@@ -33,6 +33,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    scripts = [ "bin/offchain" ]
+    ]
 )
